2f90b38273c43e9dda01b8abcb68784463b1542b
stianaske <stianaske@gmail.com>
2016-05-29 20:08:59 +0200

    Update setup.py
    Removed reference to README.rst.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-with open('README.rst') as f:
-    readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
@@ -10,7 +8,6 @@ setup(
     name='pybotvac',
     version='0.0.1',
     description='Python package for controlling Neato pybotvac Connected vacuum robot',
-    long_description=readme,
     author='Stian Askeland',
     author_email='stianaske@gmail.com',
     url='https://github.com/stianaske/pybotvac',

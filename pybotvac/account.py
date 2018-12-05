ba59c9fe4a21f047b87ec76714abc8f4531ab905
stianaske <stianaske@gmail.com>
2018-12-05 06:36:15 +0100

    Resolve #27

--- pybotvac/account.py
+++ pybotvac/account.py
@@ -83,9 +83,7 @@ class Account:
         """
         for robot in self.robots:
             resp2 = (
-                requests.get(urllib.parse.urljoin(self.ENDPOINT,
-                                                  'users/me/robots/{}/maps'
-                                                  .format(robot.serial)),
+                requests.get(urljoin(self.ENDPOINT, 'users/me/robots/{}/maps'.format(robot.serial)),
                              headers=self._headers))
             resp2.raise_for_status()
             self._maps.update({robot.serial: resp2.json()})

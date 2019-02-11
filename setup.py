283c2fd07ada807a2e1bb07591ef83ddf04ef10f
Stian Askeland <stianaske@gmail.com>
2019-02-11 20:20:00 +0100

    Resolve #35. Moved version source to setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
-from pybotvac import __version__
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
     name='pybotvac',
-    version=__version__,
+    version='0.0.13',
     description='Python package for controlling Neato pybotvac Connected vacuum robot',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -16,5 +15,6 @@ setup(
     license='Licensed under the MIT license. See LICENSE file for details',
     packages=['pybotvac'],
     package_dir={'pybotvac': 'pybotvac'},
-    package_data={'pybotvac': ['cert/*.crt']}
+    package_data={'pybotvac': ['cert/*.crt']},
+    install_requires=['requests']
 )

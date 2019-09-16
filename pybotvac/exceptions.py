307fa7c15adc2c43d238d4ef00b56c2370071875
Daniel Shokouhi <dshokouhi@gmail.com>
2019-09-16 07:23:55 -0700

    Merge from mistake

--- pybotvac/exceptions.py
+++ pybotvac/exceptions.py
@@ -2,10 +2,19 @@ class NeatoException(Exception):
     pass
 
 class NeatoLoginException(NeatoException):
+    """
+    To indicate there is a login issue.
+    """
     pass
 
 class NeatoRobotException(NeatoException):
+    """
+    To be thrown anytime there is a robot error.
+    """
     pass
 
 class NeatoUnsupportedDevice(NeatoRobotException):
+    """
+    To be thrown only for unsupported devices.
+    """
     pass

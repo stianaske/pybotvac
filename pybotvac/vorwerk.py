24187e18abed9d717f8f2faef8beb49853cc31ef
Michael Graf <michael@trunner.de>
2023-01-02 20:50:06 +0100

    Add ksecosys.com certificate (#79)

--- pybotvac/vorwerk.py
+++ pybotvac/vorwerk.py
@@ -1,3 +1,5 @@
+import os
+
 from .neato import Vendor
 
 
@@ -9,3 +11,4 @@ class Vorwerk(Vendor):
     scope = ["openid", "email", "profile", "read:current_user", "offline_access"]
     audience = "https://mykobold.eu.auth0.com/userinfo"
     source = "vorwerk_auth0"
+    cert_path = os.path.join(os.path.dirname(__file__), "cert", "ksecosys.com.crt")

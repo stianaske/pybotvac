1dc327bcd9d59ce0c6df4f50a18ecd4970dbedc8
kalteslicht <felix.lieser@googlemail.com>
2026-04-08 20:17:25 +0200

    Replace deprected pk_reources with importlib.metadata (#94)
    * Replace deprected pk_reources with importlib.metadata
    
    * Apply suggestion from @epenet
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: epenet <6771947+epenet@users.noreply.github.com>

--- pybotvac/version.py
+++ pybotvac/version.py
@@ -1,6 +1,6 @@
-import pkg_resources
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = pkg_resources.get_distribution("pybotvac").version
-except Exception:  # pylint: disable=broad-except
+    __version__ = version("pybotvac")
+except PackageNotFoundError:  # pragma: no cover - environment dependent
     __version__ = "unknown"
